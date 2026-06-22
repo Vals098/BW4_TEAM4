c0fb732fef9cfdf43e0e71193dd9b81354358c47
Vals098 <valeria.farinosi@gmail.com>
2026-06-22 17:48:23 +0200

    prima connessione DB testata

--- src/main/java/valeriafarinosi/Application.java
+++ src/main/java/valeriafarinosi/Application.java
@@ -1,6 +1,11 @@
 package valeriafarinosi;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
+
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("BW4_TEAM4");
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
