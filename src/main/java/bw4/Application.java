fb25c08766d10f9091d5ab89500093f9ff4f4c11
giulia-ciampa <giulia.ciampa7@gmail.com>
2026-06-24 09:21:01 +0200

    Update Application.java

--- src/main/java/bw4/Application.java
+++ src/main/java/bw4/Application.java
@@ -24,8 +24,9 @@ public class Application {
         PuntoVendita puntoVendita1 = new RivenditoreAutorizzato("LKI23", "Tabacchi delle fate", "Bosco Fatato");
         PuntoVendita puntoVendita2 = new DistributoreAutomatico("5KS89", "Biglietteria Ali Spiegate","Foresta Incantata", false);
 
-//        pvd.savePuntoVendita(puntoVendita1);
-//        pvd.savePuntoVendita(puntoVendita2);
+//       METODO  SAVE
+  //pvd.savePuntoVendita(puntoVendita1);
+  //pvd.savePuntoVendita(puntoVendita2);
 
 
     }
