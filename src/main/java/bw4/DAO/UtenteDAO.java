56cfaa6f286418937bf30e3b83c7becfec3ac3aa
Vals098 <valeria.farinosi@gmail.com>
2026-06-24 22:56:57 +0200

    update: exception dao utente

--- src/main/java/bw4/DAO/UtenteDAO.java
+++ src/main/java/bw4/DAO/UtenteDAO.java
@@ -64,7 +64,7 @@ public Utente findByCodiceUtente(String codiceUtente){
         Utente found = query.getSingleResult();
 
         if(found == null){
-            throw new UtenteNonTrovatoException("L'utente " + codiceUtente + " non fa parte del Fantabosco!");
+            throw new UtenteNonTrovatoException("Accipigna!! L'utente con codic " + codiceUtente + " non fa parte del Fantabosco!");
         }
 
         return found;
