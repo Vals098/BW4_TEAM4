c18e29b8c0834cfa0fa81722de846719e84e74d5
nicolorsillo <nicolorsillo@hotmail.it>
2026-06-24 11:39:55 +0200

    Aggiunte tratte in application

--- src/main/java/bw4/Application.java
+++ src/main/java/bw4/Application.java
@@ -36,6 +36,53 @@ public class Application {
   //pvd.savePuntoVendita(puntoVendita1);
   //pvd.savePuntoVendita(puntoVendita2);
 
+        //        TRATTE
+        Tratta antroDellaStregaToChiosco = new Tratta("Antro della Strega", "Chiosco", LocalTime.of(0,20));
+        Tratta antroDellaStregaToCittaLaggiu = new Tratta("Antro della Strega", "Città Laggiù", LocalTime.of(1, 30));
+        Tratta antroDellaStregaToReggiaDiReQuercia = new Tratta("Antro della Strega", "Reggia di Re Quercia", LocalTime.of(0, 40));
+        Tratta antroDellaStregaToTanaDelLupo = new Tratta("Antro della Strega", "Tana del Lupo", LocalTime.of(0, 30));
+        Tratta chioscoToAntroDellaStrega = new Tratta("Chiosco", "Antro della Strega", LocalTime.of(0, 20));
+        Tratta chioscoToCittaLaggiu = new Tratta("Chiosco", "Città Laggiù", LocalTime.of(1, 50));
+        Tratta chioscoToReggiaDiReQuercia = new Tratta("Chiosco", "Reggia di Re Quercia", LocalTime.of(1, 0));
+        Tratta chioscoToTanaDelLupo = new Tratta("Chiosco", "Tana del Lupo", LocalTime.of(0, 50));
+        Tratta cittaLaggiuToAntroDellaStrega = new Tratta("Città Laggiù", "Antro della Strega", LocalTime.of(1, 30));
+        Tratta cittaLaggiuToChiosco = new Tratta("Città Laggiù", "Chiosco", LocalTime.of(1, 50));
+        Tratta cittaLaggiuToReggiaDiReQuercia = new Tratta("Città Laggiù", "Reggia di Re Quercia", LocalTime.of(2, 10));
+        Tratta cittaLaggiuToTanaDelLupo = new Tratta("Città Laggiù", "Tana del Lupo", LocalTime.of(2, 0));
+        Tratta reggiaDiReQuerciaToAntroDellaStrega = new Tratta("Reggia di Re Quercia", "Antro della Strega", LocalTime.of(0, 40));
+        Tratta reggiaDiReQuerciaToChiosco = new Tratta("Reggia di Re Quercia", "Chiosco", LocalTime.of(1, 0));
+        Tratta reggiaDiReQuerciaToCittaLaggiu = new Tratta("Reggia di Re Quercia", "Città Laggiù", LocalTime.of(2, 10));
+        Tratta reggiaDiReQuerciaToTanaDelLupo = new Tratta("Reggia di Re Quercia", "Tana del Lupo", LocalTime.of(0, 10));
+        Tratta tanaDelLupoToAntroDellaStrega = new Tratta("Tana del Lupo", "Antro della Strega", LocalTime.of(0, 30));
+        Tratta tanaDelLupoToChiosco = new Tratta("Tana del Lupo", "Chiosco", LocalTime.of(0, 50));
+        Tratta tanaDelLupoToCittaLaggiu = new Tratta("Tana del Lupo", "Città Laggiù", LocalTime.of(2, 0));
+        Tratta tanaDelLupoToReggiaDiReQuercia = new Tratta("Tana del Lupo", "Reggia di Re Quercia", LocalTime.of(0, 10));
+        Tratta eliminabile = new Tratta("Eliminabile", "Eliminabile", LocalTime.of(0, 30));
+//        td.save(antroDellaStregaToChiosco);
+//        td.save(antroDellaStregaToCittaLaggiu);
+//        td.save(antroDellaStregaToReggiaDiReQuercia);
+//        td.save(antroDellaStregaToTanaDelLupo);
+//        td.save(chioscoToAntroDellaStrega);
+//        td.save(chioscoToCittaLaggiu);
+//        td.save(chioscoToReggiaDiReQuercia);
+//        td.save(chioscoToTanaDelLupo);
+//        td.save(cittaLaggiuToAntroDellaStrega);
+//        td.save(cittaLaggiuToChiosco);
+//        td.save(cittaLaggiuToReggiaDiReQuercia);
+//        td.save(cittaLaggiuToTanaDelLupo);
+//        td.save(reggiaDiReQuerciaToAntroDellaStrega);
+//        td.save(reggiaDiReQuerciaToChiosco);
+//        td.save(reggiaDiReQuerciaToCittaLaggiu);
+//        td.save(reggiaDiReQuerciaToTanaDelLupo);
+//        td.save(tanaDelLupoToAntroDellaStrega);
+//        td.save(tanaDelLupoToChiosco);
+//        td.save(tanaDelLupoToCittaLaggiu);
+//        td.save(tanaDelLupoToReggiaDiReQuercia);
+//        td.save(eliminabile);
+//        System.out.println(td.findById("48e48cee-d0bc-4c93-973a-d9a82a20e585"));
+//        td.deleteById("48e48cee-d0bc-4c93-973a-d9a82a20e585");
+
+
 
 
 
