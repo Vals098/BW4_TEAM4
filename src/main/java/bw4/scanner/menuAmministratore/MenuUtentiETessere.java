00b6b136671fecacefb6f1a8fb40a692981aa4b1
Saverio Casabianca <Saveriocoppola97@virgilio.it>
2026-06-25 17:36:24 +0200

    .

--- src/main/java/bw4/scanner/menuAmministratore/MenuUtentiETessere.java
+++ src/main/java/bw4/scanner/menuAmministratore/MenuUtentiETessere.java
@@ -1,10 +1,20 @@
 package bw4.scanner.menuAmministratore;
 
+import bw4.DAO.TesseraDAO;
+import bw4.DAO.UtenteDAO;
+
 import java.util.Scanner;
 
 public class MenuUtentiETessere {
 
     private final Scanner scanner = new Scanner(System.in);
+    private final UtenteDAO ud;
+    private final TesseraDAO tesseraDAO;
+
+    public MenuUtentiETessere(UtenteDAO ud, TesseraDAO tesseraDAO) {
+        this.ud = ud;
+        this.tesseraDAO = tesseraDAO;
+    }
 
     public void start() {
         boolean inSessione = true;
@@ -13,4 +23,6 @@ public class MenuUtentiETessere {
             System.out.println("\n=== MENU Utenti e Tessere ===\n");
         }
     }
+
+
 }
