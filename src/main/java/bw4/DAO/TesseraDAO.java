4a782756ed34d812fd1c59d84402dc95dd029e8b
Saverio Casabianca <Saveriocoppola97@virgilio.it>
2026-06-25 13:55:49 +0200

    prova metodo nel main

--- src/main/java/bw4/DAO/TesseraDAO.java
+++ src/main/java/bw4/DAO/TesseraDAO.java
@@ -10,10 +10,11 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 public class TesseraDAO {
-    private static EntityManager em;
+    // CORRETTO: Rimosso lo 'static' per una corretta gestione del ciclo di vita JPA
+    private final EntityManager em;
 
     public TesseraDAO(EntityManager em) {
-        TesseraDAO.em = em;
+        this.em = em;
     }
 
     public void save(Tessera tessera) {
@@ -35,56 +36,36 @@ public class TesseraDAO {
         return em.find(Tessera.class, id);
     }
 
-    // METODO CONTROLLO SCADENZA TESSERA DATO NUMERO TESSERA
-    public static boolean isValid(int numeroTessera) {
+    public void controllaERinnova(int numeroTessera) {
         try {
             TypedQuery<Tessera> query = em.createQuery(
                     "SELECT t FROM Tessera t WHERE t.numeroTessera = :numeroTessera",
                     Tessera.class);
             query.setParameter("numeroTessera", numeroTessera);
             Tessera tessera = query.getSingleResult();
-
             if (tessera.getDataDiScadenza().isBefore(LocalDate.now())) {
                 System.out.println("Per le verruche della mia bisnonna! Tessera scaduta!");
-                return false;
-            }
-            System.out.println("Che strabiliante meraviglia! Tessera valida!");
-            return true;
-        } catch (NoResultException e) {
-            System.out.println("Accipigna! Nessuna tessera trovata con numero: " + numeroTessera);
-            return false;
-        }
-    }
-
-    // METODO RINNOVO TESSERA
-    public void rinnovaTessera(int numeroTessera) {
-        try {
-            TypedQuery<Tessera> query = em.createQuery(
-                    "SELECT t FROM Tessera t WHERE t.numeroTessera = :numeroTessera",
-                    Tessera.class);
-            query.setParameter("numeroTessera", numeroTessera);
-            Tessera tessera = query.getSingleResult();
-
-            if (tessera.getDataDiScadenza().isBefore(LocalDate.now())) {
+                System.out.println("La tessera " + numeroTessera + " è scaduta.");
+                System.out.println("Procedo automaticamente con la richiesta di rinnovo...");
                 EntityTransaction transaction = em.getTransaction();
                 try {
                     transaction.begin();
-
                     tessera.setDataDiEmissione(LocalDate.now());
                     tessera.setDataDiScadenza(LocalDate.now().plusYears(1));
                     em.merge(tessera);
-
                     transaction.commit();
                     System.out.println("Che strabiliante meraviglia! La tessera numero " + numeroTessera + " è stata rinnovata!");
                 } catch (Exception ex) {
                     if (transaction.isActive()) transaction.rollback();
                     System.err.println("Errore durante il salvataggio del rinnovo: " + ex.getMessage());
                 }
-            } else {
-                System.out.println("Per mille pentole magiche! La tessera è ancora valida!");
+            }
+            else {
+                System.out.println("Che strabiliante meraviglia! Tessera valida!");
+                System.out.println("La tessera " + numeroTessera + " adesso è attiva.");
             }
         } catch (NoResultException e) {
-            System.out.println("Impossibile rinnovare: nessuna tessera trovata con numero: " + numeroTessera);
+            System.out.println("Accipigna! Nessuna tessera trovata con numero: " + numeroTessera);
         }
     }
 }
