2fed6a1c3ccb35065cd53da0b8737fb6f7f16e28
Saverio Casabianca <Saveriocoppola97@virgilio.it>
2026-06-25 18:22:37 +0200

    fixare Menu utentueTessere

--- src/main/java/bw4/scanner/MenuAmministratore.java
+++ src/main/java/bw4/scanner/MenuAmministratore.java
@@ -23,14 +23,14 @@ public class MenuAmministratore {
     public MenuAmministratore(
             AbbonamentoDAO ad,
             PuntoVenditaDAO pvd,
-                              MezzoDAO md,
-                              TrattaDAO td,
-                              PercorrenzaDAO pd,
-                              UtenteDAO ud,
-                              TesseraDAO tesseraDAO,
-                              TitoloDiViaggioDAO tvd,
-                              BigliettoDAO bd,
-                              ManutenzioneDAO manutenzioneDAO) {
+            MezzoDAO md,
+            TrattaDAO td,
+            PercorrenzaDAO pd,
+            UtenteDAO ud,
+            TesseraDAO tesseraDAO,
+            TitoloDiViaggioDAO tvd,
+            BigliettoDAO bd,
+            ManutenzioneDAO manutenzioneDAO) {
 
         this.pvd = pvd;
         this.md = md;
